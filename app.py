8f5670bc8647bf754c0640cc3cf1130ab7c7b755
samrithar <rsamritha.work@gmail.com>
2025-03-19 20:42:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import gradio as gr
+import requests
+
+def fetch_summary(company):
+    response = requests.get(f"http://127.0.0.1:5000/fetch-news?company={company}")
+    data = response.json()
+    
+    return data["articles"], data["comparative_analysis"], data["audio"]
+
+ui = gr.Interface(
+    fn=fetch_summary,
+    inputs=gr.Textbox(label="Enter Company Name"),
+    outputs=[gr.Textbox(label="Articles"), gr.Textbox(label="Comparative Analysis"), gr.Audio(label="Hindi TTS Output")]
+)
+
+ui.launch()
 
